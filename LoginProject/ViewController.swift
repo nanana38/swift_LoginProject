7162e03081d252c68e77b1c309fb174018831481
nanana38 <hyeoni_yoo@naver.com>
2022-07-07 20:57:21 +0900

    [swift] LoginProject_autolayout

--- LoginProject/ViewController.swift
+++ LoginProject/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let emailTextFieldView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.darkGray
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        setUpUI()
+
+    }
+    
+    func setUpUI() {
+
+        view.addSubview(emailTextFieldView)
+        
+        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+
     }
 
 
